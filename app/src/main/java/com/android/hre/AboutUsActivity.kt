bceaf88e09fcf6d1104716397b6d3fb7a6fd7d5e
shashi <shashi@netiapps.com>
2023-09-27 15:53:18 +0530

    about us

--- app/src/main/java/com/android/hre/AboutUsActivity.kt
+++ app/src/main/java/com/android/hre/AboutUsActivity.kt
@@ -2,10 +2,19 @@ package com.android.hre
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.hre.databinding.ActivityAboutUsBinding
 
 class AboutUsActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAboutUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_us)
+        supportActionBar?.hide()
+        //setContentView(R.layout.activity_about_us)
+
+        binding = ActivityAboutUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
